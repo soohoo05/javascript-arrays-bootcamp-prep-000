d1032c329ed05813c4f968dc5f8c5ba70ef151d4
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-26 16:21:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,4 +25,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
-function 
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
