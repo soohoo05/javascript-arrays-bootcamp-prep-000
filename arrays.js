939d2bfb6c094c9b5406d0c177edc83d12e6457e
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-26 15:57:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,4 +11,7 @@ return array
 function addElementToEndOfArray(array,element){
   return [...array,element]
 }
-function
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
