53f01beb7a1cede1164885a8f474e15c52462afb
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-26 16:15:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,4 +18,6 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array,index){
   return array[index]
 }
-function 
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  
+}
