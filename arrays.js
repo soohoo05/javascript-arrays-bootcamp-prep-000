a64441dfac289eb10d43f70d2b0549c06e6a4cb9
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-26 15:55:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ function addElementToBeginningOfArray(array, element){
 return [element,...array]  
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
-  var newArray=[element,...array]
-  return newArray
+array.unshift(element)
+return array 
 }
